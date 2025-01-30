dd36d104cce544e5d5036c59915f983bc97360d1
“Aleemuddin <aleem2747@gmail.com>
2025-01-30 13:13:02 -0600

    creating first project using spring intilizer

---
+++ firstproject/src/test/java/com/alleem/firstproject/FirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alleem.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
